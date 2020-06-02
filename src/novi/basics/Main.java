70fc3b3ac2362f2c68deea6e6467972b56da84a0
Vurgill Caster <vurgill.caster@novi-education.nl>
2020-06-02 11:35:07 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -3,6 +3,9 @@ package novi.basics;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Vurgill");
+
+        LoginController loginController = new LoginController();
+
     }
 }
